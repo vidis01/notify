a865de9f5d04e3ccdae63be75602fb955c118d20
Vidmantas <jaguaras999@gmail.com>
2022-03-30 11:22:50 +0300

    add db models

--- NotifyDB/Models/Company.cs
+++ NotifyDB/Models/Company.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace NotifyDB.Models
 {
     public class Company
     {
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        [RegularExpression(@"^[0123456789]{10}$")]
+        public string Number { get; set; }
+
+        public CompanyType Type { get; set; }
+        public Market Market { get; set; }
+        public DateTime CallDate { get; set; }
+        public List<Schedule> Schedules { get; set; }
+    }
+
+    public enum CompanyType
+    {
+        Small,
+        Medium,
+        Large
+    }
+
+    public enum Market
+    {
+        Denmark, 
+        Norway, 
+        Sweden, 
+        Finland
     }
 }
--- NotifyDB/Models/Schedule.cs
+++ NotifyDB/Models/Schedule.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NotifyDB.Models
 {
     public class Schedule
     {
+        public int Id { get; set; }
+        public Guid CompanyId { get; set; }
+        public Company Company { get; set; }
+        public DateTime NotificationDate { get; set; }
     }
 }
