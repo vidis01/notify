f47e918c56c6e04d14615332af9435430307f74a
Vidmantas <jaguaras999@gmail.com>
2022-04-01 17:51:17 +0300

    add some fixes

--- NotifyAPI/DtoModel/CompanyDto.cs
+++ NotifyAPI/DtoModel/CompanyDto.cs
@@ -6,11 +6,13 @@ namespace NotifyAPI.DtoModel
 {
     public class CompanyDto
     {
+        [Required]
         [MaxLength(100)]
         public string Name { get; set; }
 
+        [Required]
         [MaxLength(10)]
-        [RegularExpression(@"^[0123456789]{10}$", ErrorMessage = "Only numeric characters are allowed.")]
+        [RegularExpression(@"^[0123456789]{10}$", ErrorMessage = "Only numeric characters are allowed. And length must be only 10 digits.")]
         public string Number { get; set; }
 
         public CompanyType Type { get; set; }
